126af86c5068dd8fb18c7f6fa68b92c7e466dcbc
mbentivegna <mjbentivegna01@gmail.com>
2022-05-04 11:29:28 -0400

    added summary

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -5,7 +5,7 @@ function Home() {
     <div className = "total-home">
         <div className = "summary">
           <h2 className = "welcome">About Our Project</h2>
-          <p className = "aboutUs"></p>
+          <p className = "aboutUs">AURA explores the generative nature of human pathing behavior, and visualizes two key aspects of traffic: flow and density. We explore these two aspects in relation to time, analyze the utilization of space, and draw conclusions from the collected data. If you consent to the tracking of your location to participate in this demonstration, please come and take a tracking tag with you around the Houghton Gallery.</p>
         </div>
     </div>
   );
